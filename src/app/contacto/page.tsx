59fee63d27c17a8f0992e21b01e211925789578f
Rodri Otrera <rodrigo.otrerafornes@gmail.com>
2024-01-22 20:36:50 -0300

    Contacto

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -2,18 +2,23 @@
 import { padding } from "@/components/styles/padding";
 import { cn } from "@/lib/utils";
 import { Button } from "@nextui-org/react";
-import { MailIcon } from "lucide-react";
+import { InstagramIcon, MailIcon } from "lucide-react";
 
 const ContactoPage = () => {
   return (
     <div className={cn("min-h-screen pt-40", padding)}>
       <div className="h-[70vh] w-full bg-neutral-950 rounded-xl px-8 py-6">
-        <h1 className="font-bold tracking-wide text-xl">EPDRAGO</h1>
-        <h1 className="font-bold tracking-wide mt-2 text-neutral-300">
-          Telefono - 11 6621 9348
-        </h1>
-        <h1 className="font-bold tracking-wide text-xl mt-4 ">INSTAGRAMS</h1>
-        <div className="flex space-x-1">
+        <div className="flex items-center mt-8">
+          <h1 className="font-bold text-2xl tracking-wide  ">
+            Epdrago - Contactos
+          </h1>
+        </div>
+        <div className="flex items-center mt-8">
+          {" "}
+          <InstagramIcon className="text-green-500" />
+          <h1 className="font-bold text-xl tracking-wide ml-2 ">Instagrams</h1>
+        </div>{" "}
+        <div className="flex space-x-4">
           {" "}
           <Button className="mt-2" color="success" variant="bordered">
             @Epdrago
@@ -22,10 +27,9 @@ const ContactoPage = () => {
             @Axel
           </Button>{" "}
           <Button className="mt-2" color="success" variant="bordered">
-            @Epdrago
+            @Ari
           </Button>
         </div>
-
         <div className="flex flex-col  mt-8">
           <h1 className="font-bold tracking-wide text-2xl">
             Unite a la Epdrago Army!
@@ -46,7 +50,7 @@ const ContactoPage = () => {
           <MailIcon className="text-green-500" />
           <h1 className="font-bold text-xl tracking-wide ml-2 ">Email</h1>
         </div>
-        <p>leri@gmail.com</p>
+        <p className="text-xl text-neutral-500">leri@gmail.com</p>
       </div>
     </div>
   );
