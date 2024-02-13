71b46d3f2bd400b043fa840cab3013391753f068
Rodri Otrera <rodrigo.otrerafornes@gmail.com>
2024-02-13 05:03:43 -0300

    FAIL

--- src/lib/db/schema/news.ts
+++ src/lib/db/schema/news.ts
@@ -6,5 +6,9 @@ export const news = pgTable('news ', {
     content: text('content').notNull(),
     createdAt: timestamp('createdAt').defaultNow(),
     img_url: text('img_url').notNull(),
+<<<<<<< HEAD
     link: text("text")
+=======
+    link: text("link")
+>>>>>>> parent of 87de9de (Romove error)
 })
