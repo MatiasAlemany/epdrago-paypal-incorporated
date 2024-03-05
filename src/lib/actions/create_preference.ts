223ebfe67f8fe38e058f1354b367570c50445610
rodriotrera <rodrigo.otrerafornes@gmail.com>
2024-03-05 17:32:36 -0300

    To PC

--- src/lib/actions/create_preference.ts
+++ src/lib/actions/create_preference.ts
@@ -28,6 +28,7 @@ export const createPreferenceResponse = async ({ item_id, price, title, metadata
             failure: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/payment/error' : `https://axeldubin.com/payment/error`,
         },
         binary_mode: true,
+        notification_url: "https://epdrago.com.ar/api/webhooks/payments",
         items: [
             {
                 id: item_id,
