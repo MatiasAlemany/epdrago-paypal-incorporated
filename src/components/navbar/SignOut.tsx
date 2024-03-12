e4715aafeb1344daa6f5a85940ebf96a334e05b5
Rodri Otrera <rodrigo.otrerafornes@gmail.com>
2024-03-12 20:15:54 -0300

    Navbar FIX2

--- src/components/navbar/SignOut.tsx
+++ src/components/navbar/SignOut.tsx
@@ -12,7 +12,7 @@ const SignOut = () => {
   return (
     <DropdownMenuItem
       onClick={async () => {
-        signOut(() => {
+      await signOut(() => {
           router.refresh();
           router.push("/");
         });
