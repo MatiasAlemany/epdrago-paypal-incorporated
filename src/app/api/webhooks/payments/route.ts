8f2336ca4633d33d4a1e2971ed14ddcf0c677ade
Rodri Otrera <rodrigo.otrerafornes@gmail.com>
2024-08-25 20:16:59 -0300

    MP FIXED

--- src/app/api/webhooks/payments/route.ts
+++ src/app/api/webhooks/payments/route.ts
@@ -37,59 +37,58 @@ async function handler(req: NextRequest) {
                 }
             })).data;
             if (payment.status_detail == 'accredited') {
-                console.log('Got a payment');
+                const metadata = payment.metadata as MetadataPreference;
+                const courseRelation = (await db.select().from(usersToCourses).where(and(eq(usersToCourses.course_id, metadata.product_id), eq(usersToCourses.user_id, metadata.user_id))));
+
+                if (courseRelation.length > 0) {
+                    console.log(courseRelation);
+                    console.log("User already has course")
+                    return NextResponse.json({ message: "Payment already exists" }, { status: 200 });
+
+                }
+
+
+                const paymentInsertValues: InsertPayment = {
+                    id: payment.id!,
+                    item_title: metadata.product_title,
+                    net_amount: Math.round(payment.transaction_details!.net_received_amount!),
+                    payerName: payment.payer!.first_name,
+                    course_id: metadata.product_id,
+                    user_id: metadata.user_id,
+                }
+
+
+                try {
+                    await db.insert(payment_schema).values(paymentInsertValues);
+                    await db.insert(usersToCourses).values({ course_id: metadata.product_id, user_id: metadata.user_id });
+                    const firstModule = await getFirstModuleOfCourse(metadata.product_id);
+                    if (firstModule == undefined) {
+                        throw Error('First Module not found!');
+                    }
+                    await db.insert(course_progress).values({
+                        isFinished: false,
+                        module_number: 0,
+                        user_id: metadata.user_id,
+                        course_id: metadata.product_id,
+                        module_id: firstModule.id
+                    })
+
+
+                    console.log('course bought!!');
+
+                    return NextResponse.json({ message: "Payment Succesfull!!" }, { status: 200 });
+
+                } catch (error) {
+                    console.log(error);
+                    return NextResponse.json({ message: "Error saving on db" }, { status: 400 });
+
+                }
             } else {
                 console.log('Got a payment but its not accredited!');
 
             }
 
-            console.log("Payment ID", payment.id);
-            const metadata = payment.metadata as MetadataPreference;
-            const courseRelation = (await db.select().from(usersToCourses).where(and(eq(usersToCourses.course_id, metadata.product_id), eq(usersToCourses.user_id, metadata.user_id))));
 
-            if (courseRelation.length > 0) {
-                console.log(courseRelation);
-                console.log("User already has course")
-                return NextResponse.json({ message: "Payment already exists" }, { status: 200 });
-
-            }
-
-
-            const paymentInsertValues: InsertPayment = {
-                id: payment.id!,
-                item_title: metadata.product_title,
-                net_amount: Math.round(payment.transaction_details!.net_received_amount!),
-                payerName: payment.payer!.first_name,
-                course_id: metadata.product_id,
-                user_id: metadata.user_id,
-            }
-
-
-            try {
-                await db.insert(payment_schema).values(paymentInsertValues);
-                await db.insert(usersToCourses).values({ course_id: metadata.product_id, user_id: metadata.user_id });
-                const firstModule = await getFirstModuleOfCourse(metadata.product_id);
-                if (firstModule == undefined) {
-                    throw Error('First Module not found!');
-                }
-                await db.insert(course_progress).values({
-                    isFinished: false,
-                    module_number: 0,
-                    user_id: metadata.user_id,
-                    course_id: metadata.product_id,
-                    module_id: firstModule.id
-                })
-
-
-                console.log('course bought!!');
-
-                return NextResponse.json({ message: "Payment Succesfull!!" }, { status: 200 });
-
-            } catch (error) {
-                console.log(error);
-                return NextResponse.json({ message: "Error saving on db" }, { status: 400 });
-
-            }
 
 
         } catch (error) {
